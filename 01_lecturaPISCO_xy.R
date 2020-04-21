825798cd9224c97a7fd0ab96982d311906f5bbc1
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2020-04-21 14:46:56 -0500

    Update 01_lecturaPISCO_xy.R

--- 01_lecturaPISCO_xy.R
+++ 01_lecturaPISCO_xy.R
@@ -12,7 +12,5 @@ raster::projection(long_lat) <- raster::projection(raster_pp)
 points_long_lat <- raster::extract(raster_pp[[1]], long_lat, cellnumbers = T)[,1]
 data_long_lat <- t(raster_pp[points_long_lat])
 colnames(data_long_lat) <- as.character(long_lat$NN)
-
-
 save.image("D:/2_Courses/R_Hidrologia/Tutorial_files/data_long_lat.csv")
 write.csv(data_long_lat, "data_long_lat.csv", quote = F)
