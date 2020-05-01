d065988139c320193ee3ed5c93e3aee42b0800e1
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2020-05-01 13:56:29 -0500

    Update 01_lecturaPISCO_xy.R

--- 01_lecturaPISCO_xy.R
+++ 01_lecturaPISCO_xy.R
@@ -1,4 +1,4 @@
-### Extraccion de datos de una coordenada desde un raster
+### Extraccion de datos desde una coordenada de un raster
 ### https://github.com/hydrocodes
 setwd("D:/2_Courses/R_Hidrologia/Tutorial_files")  
 rm(list = ls())
