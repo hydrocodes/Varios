d66726f91150f0add355efe993922729ad60f6a6
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2020-10-11 10:33:56 -0500

    Update 05_lectura_shp_arid.R

--- 05_lectura_shp_arid.R
+++ 05_lectura_shp_arid.R
@@ -4,7 +4,7 @@ rm(list=ls())
 dev.off()
 library(raster)
 library(rgdal)
-shpfile <- readOGR("C:/6_VARIOS/10_Sites/3_Blog/sig/aridez_peru/peru_aridez_regiones.shp") #insérer le bassin shp
+shpfile <- readOGR("C:/6_VARIOS/10_Sites/3_Blog/sig/aridez_peru/peru_aridez_regiones.shp")
 head(shpfile)
 all_shp_colors <- c("red", "yellow", "orange", "green", "white")[shpfile$DN]
 l_palette <- c("red", "yellow", "orange", "green", "white")
@@ -14,7 +14,7 @@ plot(shpfile,
      main = "Mapa de aridez del Perú",
      axes=T, xlab="Longitud", ylab="Latitud"
      )
-text(-82,-9,"Océano\nPacifico", cex=0.8)
+text(-82,-9,"Océano\nPacífico", cex=0.8)
 legend(-86,-11.5,
        legend = c("hiperárido", "árido", "semiárido", "subhúmedo seco"),
        fill = l_palette,
