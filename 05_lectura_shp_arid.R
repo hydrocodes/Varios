c49918ffb3f5aecc1439b01a4cc25d15cfdfb41e
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2020-10-11 10:26:35 -0500

    Update 05_lectura_shp_arid.R

--- 05_lectura_shp_arid.R
+++ 05_lectura_shp_arid.R
@@ -1,26 +1,23 @@
-### Plotear un shp, caso mapa de aridez del Peru
+### Plotear un shp, caso mapa de aridez del Peru (Rau, 2019)
 ### https://github.com/hydrocodes
 rm(list=ls())
 dev.off()
 library(raster)
 library(rgdal)
-shpfile <- readOGR("C:/6_VARIOS/10_Sites/3_Blog/sig/aridez_peru/peru_aridez_regiones.shp") #insérer le bassin shp
+shpfile <- readOGR("C:/6_VARIOS/10_Sites/3_Blog/sig/aridez_peru/peru_aridez_regiones.shp") #insÃ©rer le bassin shp
 head(shpfile)
 all_shp_colors <- c("red", "yellow", "orange", "green", "white")[shpfile$DN]
 l_palette <- c("red", "yellow", "orange", "green", "white")
 par(mar = c(4, 4, 3, 2)) 
 plot(shpfile,
      col = all_shp_colors,
-     main = "Mapa de aridez del Perú",
+     main = "Mapa de aridez del PerÃº",
      axes=T, xlab="Longitud", ylab="Latitud"
      )
-text(-82,-9,"Océano\nPacifico", cex=0.8)
+text(-82,-9,"OcÃ©ano\nPacifico", cex=0.8)
 legend(-86,-11.5,
-       legend = c("hiperárido", "árido", "semiárido", "subhúmedo seco"),
+       legend = c("hiperÃ¡rido", "Ã¡rido", "semiÃ¡rido", "subhÃºmedo seco"),
        fill = l_palette,
        bty="n",
        cex=0.9)
-
-#order(unique(shpfile$DN)
-#lapply(shpfile$DN, levels)
 
