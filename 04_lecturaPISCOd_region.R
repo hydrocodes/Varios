d325415d9323bbac360645d46afddc0b079581b3
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2020-10-17 19:01:26 -0500

    Update 04_lecturaPISCOd_region.R

--- 04_lecturaPISCOd_region.R
+++ 04_lecturaPISCOd_region.R
@@ -1,4 +1,4 @@
-### Extraccion de datos desde una region de un raster diario
+### Extraccion de la precipitacion diaria desde un raster delimitado por una region poligono shapefile
 ### https://github.com/hydrocodes
 library(sp)
 library(rgdal)
