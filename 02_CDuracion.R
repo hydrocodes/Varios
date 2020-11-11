1d3aea8a0c7f41b5f3dea25216e44e4167d0290e
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2020-11-11 10:07:44 -0500

    Update 02_CDuracion.R

--- 02_CDuracion.R
+++ 02_CDuracion.R
@@ -6,9 +6,16 @@ dev.off()
 library(waterData)
 library(hydroTSM)
 library(RCurl)
+# Opcion 1 - Leyendo archivo csv desde google drive
+# Ingresar el ID del archivo compartido abiertamente
+id <- "1A4XEx0h1qAbFq-CTKcvmg_unA8Q6JRHV"
+gdata <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
+qdiario <- gdata$Qd_m3s
+# Opcion 2 - Leyendo archivo csv desde github
 p<-getURL("https://raw.githubusercontent.com/hydrocodes/Varios/master/02_caudales.csv")
 datos<-read.csv(text = p)
 qdiario <- datos$Qd_m3s
+
 fdc(qdiario,
     lQ.thr=0.9, hQ.thr=0.1,
     plot=TRUE, log="y",
