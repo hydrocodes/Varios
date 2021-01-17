1f3323cbe69fbe2cd967aa13248f1161ca042b42
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2021-01-17 09:33:49 -0500

    Update 07_rindex.R

--- 07_rindex.R
+++ 07_rindex.R
@@ -1,17 +1,18 @@
-### Monthly runoff index based on Eq. 8 from Rau et al (2019)
+### Monthly runoff index for ungauged basins
+### Based on Eq. 8 from Rau et al (2019)
 ### https://doi.org/10.1002/hyp.13318
 ### https://github.com/hydrocodes
 rm(list=ls())
 dev.off()
 library(ggplot2)
-# Enter a csv monthly database: %b-%y(Date), Precipitation (P) and Potential Evapotranspiration (PET) in mm
+#Enter a csv monthly database: %b-%y(Date), Precipitation (P) and Potential Evapotranspiration (PET) in mm
 data <- read.csv("07_rimac_ch.csv",header=TRUE, check.names = F, stringsAsFactors = F)
-# Enter morphometric parameters of the basin
-a <-2352   # Area in km2
-l <- 88.3  # Main channel lenght in km
-p <- 261   # Perimeter in km
+#Enter morphometric parameters of the basin
+a <-2352   #Area in km2
+l <- 88.3  #Main channel lenght in km
+p <- 261   #Perimeter in km
 
-#### Do not change or move ####
+#### Do not change ####
 X1 <- (a^0.393*l^-4.107*p^4.291)/64.5
 X2 <- 0.883*a^0.369*l^-0.229*p^-0.168
 X1o <- X1/2
@@ -50,22 +51,16 @@ for (i in 2:nrow(data)) {
   R <- R2-Q
   }
 data$Qref <- Q
-Qvector<-as.vector(t(Q))
-#### end1 ####
+Qmatrix <- t(matrix(Q,12))
+#### end ####
 
-#Enter the start and end of the dates (year, month)
-Qts<-stats::ts(Qvector, start=c(1970, 1), end=c(2009, 12), frequency=12)
-
-#### end 2 ####
-dmn <- list(month.abb, unique(floor(time(Qts))))
-Qdf <- as.data.frame(t(matrix(Qts, 12, dimnames = dmn)))
-res <- scale(Qdf)
+res <- scale(Qmatrix)
 attributes(res)[3:4] <- NULL
 res_vector<-as.vector(t(res))
 data$RIndex <- res_vector
 data$Date <- as.POSIXct(paste("01", data$Date, sep = "-"), format = "%d-%b-%y")
 
-# Plotting time series
+#Plotting time series
 theme_set(theme_bw())
 ggplot(data, aes(x = Date, y = RIndex, fill = RIndex >=0)) +
   geom_col(position = "identity")+
@@ -74,5 +69,5 @@ ggplot(data, aes(x = Date, y = RIndex, fill = RIndex >=0)) +
   theme(axis.text=element_text(size=12),
         axis.title=element_text(size=12),
         plot.title=element_text(size=14))
-# Writing output file
+#Writing output file
 write.csv(data,"output.csv")
