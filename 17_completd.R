5897600922efd70406351c5da3bc15324a31972a
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2021-01-26 15:30:22 -0500

    Update 17_completd.R

--- 17_completd.R
+++ 17_completd.R
@@ -6,9 +6,9 @@ rm(list=ls())
 dev.off()
 library(cutoffR)
 # Ingresar una base de datos en csv: %d-%b-%y (date), datos diarios (cabecera con codigo de estaciones)
-data <- read.csv("C:/8_COURSES/R_Hidrologia/Rfiles_base/completacion/data0.csv",header=TRUE, check.names = F, stringsAsFactors = F)
+data <- read.csv(".../datad.csv",header=TRUE, check.names = F, stringsAsFactors = F)
 # Ingresar la ruta y el nombre del archivo de salida
-output <- "C:/8_COURSES/R_Hidrologia/Rfiles_base/completacion/completd.csv"
+output <- ".../completd.csv"
 
 # Funcion de completacion
 completd <- function(data)
