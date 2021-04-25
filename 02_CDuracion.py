b4010f6acf86fd7d704305e23ef6328455d53139
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2021-04-25 10:32:53 -0500

    Create 02_CDuracion.py

---
+++ 02_CDuracion.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = pd.read_csv('02_caudales.csv', usecols=['Fecha','Qd_m3s'])
+sort = data.sort_values(['Qd_m3s'], ascending=[False])
+y = sort.Qd_m3s
+exceedence = np.arange(1.,len(sort)+1) / len(sort)
+plt.plot(exceedence*100, y)
+plt.xlabel("Excedencia (%)")
+plt.ylabel("Descarga (m3/s)")
+plt.show()
