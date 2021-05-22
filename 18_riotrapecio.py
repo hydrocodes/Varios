ce1b67beb1291e9a0d64aec7b305ae2ce9a49a8d
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2021-05-21 22:06:21 -0500

    Update 18_riotrapecio.py

--- 18_riotrapecio.py
+++ 18_riotrapecio.py
@@ -3,10 +3,10 @@
 import numpy as np
 from scipy.optimize import fmin
 # definiendo las variables
-Q = 8 #caudal m3/s
+Q = 8 # caudal m3/s
 n = 0.025 # coeficiente de Manning
 S = 0.01 # pendiente
-b = 10 # ancho de fondo
+b = 10 # ancho de fondo m
 z = 2 # talud zH:1V
 # definiendo la funcion del caudal
 def flow(y):
@@ -14,8 +14,8 @@ def flow(y):
  epsilon = np.abs(Q_est - Q)
  return epsilon
 y_optimum = fmin(flow,0.2)
-print("tirante =",y_optimum)
+print("tirante =",y_optimum,"m")
 vel = Q/((b+z*y_optimum)*y_optimum)
-print("velocidad =",vel)
+print("velocidad =",vel,"m/s")
 Fr = vel/(9.81*((b+z*y_optimum)*y_optimum)/(b+2*z*y_optimum))**0.5
 print("Nro de Froude =",Fr)
