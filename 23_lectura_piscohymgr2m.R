23f624243b31c631e7533edfebb2249f92acbd29
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2021-10-25 12:06:32 -0500

    Create 23_lectura_piscohymgr2m.R

---
+++ 23_lectura_piscohymgr2m.R
@@ -0,0 +1,8 @@
+### Lectura de netcdf producto PISCO-HyM-GR2m
+### Llauca et al (2021) https://doi.org/10.3390/w13081048
+### https://github.com/hydrocodes
+library(raster)
+r <- stack("C:/.../pisco_hym.nc")
+Qm <- rev(r[,93]) # indicar el ID de la cuenca despues de la coma (ver shapefiles de cuencas y rios)
+Qmd <- ts(Qm, start = c(1981,1), freq=12)
+plot.ts(Qmd, ylab="Streamflow (m3/s)")
