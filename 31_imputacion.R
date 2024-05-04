be2f0659d8fb0a9a1376af288f507851e29773e0
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2024-05-04 09:48:19 -0500

    Create 31_imputacion.R

---
+++ 31_imputacion.R
@@ -0,0 +1,31 @@
+### Imputacion simple de datos
+### https://github.com/hydrocodes
+
+### Metodo del promedio mensual
+data <- read.table(file.choose(), header=T, sep="\t") #leer txt con estaciones A y B
+estacionA <- data$A
+resA <- cbind.data.frame(split(estacionA, rep(1:12, times=length(estacionA)/12)), stringsAsFactors=F)
+names(resA) <- c("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic")
+for (i in 1:ncol(resA)) {
+  resA[is.na(resA[,i]), i] <- mean(resA[,i], na.rm=T)
+}
+View(resA)
+
+### Metodo por regresion lineal
+estacionB <- data$B
+resB <- cbind.data.frame(split(estacionB, rep(1:12, times=length(estacionB)/12)), stringsAsFactors=F)
+names(resB) <- c("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic")
+View(resA)
+View(resB)
+#Completando vacios de Diciembre en A y B
+compl <- lm(resA$Dic ~ resB$Dic)
+summary(compl)
+new_resA_Dic <- compl$coefficients[2]*resB$Dic + compl$coefficients[1]
+resA$Dic[is.na(resA$Dic)] <- new_resA_Dic[is.na(resA$Dic)] #Completando Dic A
+View(resA)
+
+compl <- lm(resB$Dic ~ resA$Dic)
+summary(compl)
+new_resB_Dic <- compl$coefficients[2]*resA$Dic + compl$coefficients[1]
+resB$Dic[is.na(resB$Dic)] <- new_resB_Dic[is.na(resB$Dic)] #Completando Dic B
+View(resB)
