e84d34b860f2414fd082e2d5c1fdf30121613602
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2024-06-08 14:23:37 -0500

    Update 33_drought_sdi.R

--- 33_drought_sdi.R
+++ 33_drought_sdi.R
@@ -7,8 +7,8 @@ sdi <- SDI(data$V1, ts = 6, dist = "EmpGrin") # enter ts in months
 View(sdi$SDI)
 sdi_v <- as.vector(t(sdi$SDI))
 idx <- seq(as.Date('1981-01-01'),as.Date('2022-12-31'), by = "month")
-df <- data.frame(Date=idx,SPI=sdi_v) 
-ggplot(df, aes(x = Date, y = SPI, fill = SPI >=0)) +
+df <- data.frame(Date=idx,SSI=sdi_v) 
+ggplot(df, aes(x = Date, y = SSI, fill = SSI >=0)) +
   geom_col(position = "identity")+
   scale_fill_manual(values = c("red", "blue"), guide = "none")+
   theme(axis.text=element_text(size=12),
