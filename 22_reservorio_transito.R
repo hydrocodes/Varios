532f82ef1c7e320595458a4ef5bfd9ae3f8a88dd
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2024-06-08 19:26:33 -0500

    Update 22_reservorio_transito.R

--- 22_reservorio_transito.R
+++ 22_reservorio_transito.R
@@ -1,9 +1,9 @@
 ### Transito de avenidas en resevorios - Método de la Piscina Nivelada o Puls modificado
 ### https://github.com/hydrocodes
 library(RHMS)
-data1 <- read.table(".../input.txt", header=TRUE)  #leer hidrograma de entrada
-data2 <- read.table(".../sc.txt", header=TRUE) #leer datos elevacion m vs almacenamiento MMC
-data3 <- read.table(".../dc.txt", header=TRUE) #leer datos elevacion m vs caudal por vertedero m3/s
+data1 <- read.table("https://raw.githubusercontent.com/hydrocodes/Varios/master/22_input.txt", header=TRUE)  #leer hidrograma de entrada
+data2 <- read.table("https://raw.githubusercontent.com/hydrocodes/Varios/master/22_sc.txt", header=TRUE) #leer datos elevacion m vs almacenamiento MMC
+data3 <- read.table("https://raw.githubusercontent.com/hydrocodes/Varios/master/22_dc.txt", header=TRUE) #leer datos elevacion m vs caudal por vertedero m3/s
 
 input <- data1$input
 storageElevationCurve<-data.frame(s=data2$se,h=data2$he)
@@ -13,5 +13,5 @@ geometry<-list(storageElevationCurve=storageElevationCurve,
                capacity=5.458) # almacenamiento MMC inicio de interaciones
 simulation<-list(start='2000-01-01',end='2000-01-02',by=1800)
 reservoir_sim<-reservoirRouting(inflow=input, geometry=geometry, simulation=simulation)
-plot(reservoir_sim$operation[,1],typ="o", ylab="Discharge rate (cms)", xlab="Time step")
+plot(reservoir_sim$operation[,1],typ="o", ylab="Discharge rate (m3/s)", xlab="Time step")
 lines(reservoir_sim$operation[,3],col=2)
