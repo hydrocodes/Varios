02f66f1528b4cdf156dd1b19aa9aabbae16682f5
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2024-06-29 09:21:30 -0500

    Update 30_soil_texture.R

--- 30_soil_texture.R
+++ 30_soil_texture.R
@@ -1,5 +1,5 @@
 ### Triangulo textural de suelo
-### Julien Moeys et al (2022)
+### Moeys et al (2022)
 library(soiltexture)
 data <- data.frame(sand=15, silt=52, clay=33)
 TT.plot(class.sys = "USDA.TT", tri.data = data,
