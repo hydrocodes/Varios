5c720474392989b6edebbec6122da29c33441c6e
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2024-08-01 09:52:37 -0500

    Update 10_lectura_GCN.R

--- 10_lectura_GCN.R
+++ 10_lectura_GCN.R
@@ -1,19 +1,17 @@
-### Obtencion de la Curva Numero promedio para una region en shapefile
+### Obtencion de la Curva Numero promedio para un poligono shapefile
 ### Uso del producto GCN250 Jaafar et al (2019)
 ### https://doi.org/10.6084/m9.figshare.7756202
 ### https://github.com/hydrocodes
 rm(list=ls())
 dev.off()
 library(raster)
-library(rgdal)
-library(maps)
-topo <- raster(".../GCN250_ARCIII.tif")
-str(topo)
-shpfile <- readOGR(".../caplina.shp") #shapefile en WGS84 geo
+library(terra)
+cn <- rast("C:/.../GCN250_ARCII.tif")
+str(cn) #verificar estructura SpatRaster
+shpfile <- vect("C:/.../cuenca_caplina_ac.shp") #shapefile en WGS84 geo
 plot(shpfile)
-topo.crop <- crop(topo, shpfile, snap="out")
-plot(topo.crop)
-topo.mask <- mask(topo.crop, shpfile, snap="out")
-plot(topo.mask)
-str(topo.mask)
-cn.mean <- cellStats(topo.mask, stat='mean', na.rm=TRUE); cn.mean  #valor CN promedio
+cn.crop <- crop(cn, shpfile)
+plot(cn.crop)
+cn.mask <- mask(cn.crop, shpfile)
+plot(cn.mask)
+cn.mean <- global(cn.mask, "mean", na.rm=T);cn.mean #mostrar promedio sin considerar los vacios NA
