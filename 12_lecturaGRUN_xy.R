338d1be2e8ee6651d15363ce5f89806ef126de5e
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2024-08-06 10:48:16 -0500

    Update 12_lecturaGRUN_xy.R

--- 12_lecturaGRUN_xy.R
+++ 12_lecturaGRUN_xy.R
@@ -1,28 +1,21 @@
 ### Extraccion de la escorrentia mensual del producto GRUN (Ghiggi et al, 2019)
 ### https://github.com/hydrocodes
-rm(list = ls())
-dev.off()
-library(raster)
-library(ncdf4)
-library(sp)
-setwd("/.../")  
-#Visualizando el alcance de GRUN sobre la cuenca
-p <- raster::brick("GRUN_v1_GSWP3_WGS84_05_1902_2014.nc")
-w <- shapefile('piura_up.shp')
-r <- shapefile('rio.shp')
-raster_q <- crop(p, w, snap="out")
-plot(raster_q[[1]])
-plot(w, add=T)
-plot(r, col="blue", add=T)
-str(raster_q)
-#Lectura de archivo csv conteniendo la(s) coordenadas(s) seleccionada(s)
-long_lat <- read.csv("12_long_lat.csv", header = T)
-sp::coordinates(long_lat) <- ~XX+YY
-raster::projection(long_lat) <- raster::projection(raster_q)
-points_long_lat <- raster::extract(raster_q[[1]], long_lat, cellnumbers = T)[,1]
-data_long_lat <- t(raster_q[points_long_lat])*30.5
-colnames(data_long_lat) <- as.character(long_lat$NN)
-#Ploteando la serie de escorrentia mensual para una sola coordenada de ingreso
-#Para un grupo de coordenadas, ir directamente al archivo csv generado en la linea 28
-plot(data_long_lat, type="l",xlab="1902-2014", ylab="R (mm/mes)", main="Escorrentía media mensual")
-write.csv(data_long_lat, "12_R_GRUN.csv", quote = F)
+library(terra)
+r <- rast(".../bo_GRUN.nc")
+plot(r[[1]])
+v <- vect(".../ravelo.shp")
+plot(v, add=T)
+crop <- terra::crop(r, v, snap="out")
+plot(crop[[1]])
+plot(v, add=T)
+long_lat <- read.csv("01_long_lat.csv", header = T) #ingresar coordenadas de extraccion en el archivo csv
+long_lat$NN <- NULL
+values <- extract(crop, long_lat) 
+datos <- t(values)*30.5 # convirtiendo datos diarios a mensuales
+View(datos)
+write.csv(datos, ".../data_long_lat.csv", quote = F) # archivo de salida
+x <- ts(datos[2:nrow(datos)], start=1902, end=2014, frequency = 12)
+plot(x, type="l", ylab="Q (mm/mes)", xlab="Años")
+lines(lowess(time(x), x), col="blue", lwd=2)
+#Para el caso de cuencas que abarcan mas de una grilla. Estimando el peso de cada grilla
+weights <- extract(crop[[1]], v, cellnumbers=T,  weights=T) 
